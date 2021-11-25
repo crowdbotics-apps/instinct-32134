ecaad8f98241ea7d6c701dcfe82131cd58e4d879
Crowdbotics <team@crowdbotics.com>
2021-11-25 07:59:25 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/instinct_32134/MainActivity.java
@@ -0,0 +1,15 @@
+package com.instinct_32134;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "instinct_32134";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "instinct_32134",
+  url: "https://instinct_32134.botics.co",
+  api: "https://instinct_32134.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
